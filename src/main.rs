3dcc1d432db9c648c2591570bda5fb711fade761
orocane <s1ran@outlook.com>
2023-05-01 20:38:32 +0800

    Fix match keyword

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::{fs::read_to_string, io::Error};
 use lexer::Lexer;
+use std::{fs::read_to_string, io::Error};
 
-mod string_table;
 mod lexer;
+mod string_table;
 fn main() -> Result<(), Error> {
     let path = "test.cl";
     let text = read_to_string(path)?;
     let mut lexer = Lexer::new(&text);
 
-    println!("#name {path}");
-    while let Some((line_number, token)) = lexer.lex(){
-        lexer.print_token(line_number, token);
+    while let Some((line_number, token)) = lexer.lex() {
+        println!("{}", lexer.token_to_string(line_number, token));
     }
     Ok(())
 }
